f5ad9e8bc32312cf42263f78792f223d7b708131
FreedomBryant <1227525449@qq.com>
2019-10-21 19:38:00 +0800

    约会网站分析
    添加约会网站分析

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -37,3 +37,35 @@ def classify0(inX,dataSet,labels,k):
     print(sortedClassCount)
     return sortedClassCount[0][0]
 
+def file2matrix(filename):
+    # 打开文件
+    fr = open("./datingTestSet.txt")
+    # 读取文件的所有内容
+    arrayOLines = fr.readlines()
+    print(arrayOLines)
+    # 得到文件行数
+    numberOfLines = len(arrayOLines)
+    # 返回的Numpy矩阵numberOfLines行，3列
+    returnMat = np.zeros((numberOfLines, 3))
+    # 创建分类标签向量
+    classLabelVector = []
+    # 行的索引值
+    index = 0
+    # 读取每一行
+    for line in arrayOLines:
+        # 去掉每一行首尾的空白符，例如'\n','\r','\t',' '
+        line = line.strip()
+        # 将每一行内容根据'\t' 符进行切片，本例中一共有4列
+        listFromLine = line.split('\t')
+        # 将数据的前3列进行提取保存在returnMat矩阵中，也就是特征矩阵
+        returnMat[index, :] = listFromLine[0:3]
+        # 根据文本内容进行分类1：不喜欢 2：一般 3：喜欢
+        if listFromLine[-1] == 'didntLike':
+            classLabelVector.append(1)
+        elif listFromLine[-1] == 'smallDoses':
+            classLabelVector.append(2)
+        elif listFromLine[-1] == 'largeDoses':
+            classLabelVector.append(3)
+        index += 1
+    # 返回标签列向量以及特征矩阵
+    return returnMat,classLabelVector

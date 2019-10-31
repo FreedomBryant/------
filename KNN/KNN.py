0611e537ddc747ba0df45197ead5ecb330690601
FreedomBryant <1227525449@qq.com>
2019-10-31 18:48:40 +0800

    完成约会网站分析

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,4 +1,8 @@
 import numpy as np
+from matplotlib.font_manager import FontProperties
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+import time
 def creatDataSet():
     group = np.array([[1,1.1],[1,1],[0,0],[0,0.1]])
     labels = ['爱情片','爱情片','动作片','动作片']
@@ -69,3 +73,127 @@ def file2matrix(filename):
         index += 1
     # 返回标签列向量以及特征矩阵
     return returnMat,classLabelVector
+
+def showdatas(datingDataMat, datingLabels):
+    # 设置汉字格式为14号简体字
+    font = FontProperties(fname=r"C:\Windows\Fonts\simsun.ttc", size=14)
+    # 将fig画布分隔成1行1列，不共享x轴和y轴，fig画布的大小为（13，8）
+    # 当nrows=2，ncols=2时，代表fig画布被分为4个区域，axs[0][0]代表第一行第一个区域
+    fig, axs = plt.subplots(nrows=2, ncols=2, sharex=False, sharey=False, figsize=(13, 8))
+    # 获取datingLabels的行数作为label的个数
+    # numberOfLabels = len(datingLabels)
+    # label的颜色配置矩阵
+    LabelsColors = []
+    for i in datingLabels:
+        # didntLike
+        if i == 1:
+            LabelsColors.append('black')
+        # smallDoses
+        if i == 2:
+            LabelsColors.append('orange')
+        # largeDoses
+        if i == 3:
+            LabelsColors.append('red')
+    # 画出散点图，以datingDataMat矩阵第一列为x，第二列为y，散点大小为15, 透明度为0.5
+    axs[0][0].scatter(x=datingDataMat[:,0], y=datingDataMat[:,1], color=LabelsColors, s=15, alpha=.5)
+    # 设置标题，x轴label， y轴label
+    axs0_title_text = axs[0][0].set_title(u'每年获得的飞行常客里程数与玩视频游戏所消耗时间占比', FontProperties=font)
+    axs0_xlabel_text = axs[0][0].set_xlabel(u'每年获得的飞行常客里程数', FontProperties=font)
+    axs0_ylabel_text = axs[0][0].set_ylabel(u'玩视频游戏所消耗时间占比', FontProperties=font)
+    plt.setp(axs0_title_text, size=9, weight='bold', color='red')
+    plt.setp(axs0_xlabel_text, size=7, weight='bold', color='black')
+    plt.setp(axs0_ylabel_text, size=7, weight='bold', color='black')
+    # 画出散点图，以datingDataMat矩阵第一列为x，第三列为y，散点大小为15, 透明度为0.5
+    axs[0][1].scatter(x=datingDataMat[:,0], y=datingDataMat[:,2], color=LabelsColors, s=15, alpha=.5)
+    # 设置标题，x轴label， y轴label
+    axs1_title_text = axs[0][1].set_title(u'每年获得的飞行常客里程数与每周消费的冰淇淋公升数', FontProperties=font)
+    axs1_xlabel_text = axs[0][1].set_xlabel(u'每年获得的飞行常客里程数', FontProperties=font)
+    axs1_ylabel_text = axs[0][1].set_ylabel(u'每周消费的冰淇淋公升数', FontProperties=font)
+    plt.setp(axs1_title_text, size=9, weight='bold', color='red')
+    plt.setp(axs1_xlabel_text, size=7, weight='bold', color='black')
+    plt.setp(axs1_ylabel_text, size=7, weight='bold', color='black')
+    # 画出散点图，以datingDataMat矩阵第二列为x，第三列为y，散点大小为15, 透明度为0.5
+    axs[1][0].scatter(x=datingDataMat[:,1], y=datingDataMat[:,2], color=LabelsColors, s=15, alpha=.5)
+    # 设置标题，x轴label， y轴label
+    axs2_title_text = axs[1][0].set_title(u'玩视频游戏所消耗时间占比与每周消费的冰淇淋公升数', FontProperties=font)
+    axs2_xlabel_text = axs[1][0].set_xlabel(u'玩视频游戏所消耗时间占比', FontProperties=font)
+    axs2_ylabel_text = axs[1][0].set_ylabel(u'每周消费的冰淇淋公升数', FontProperties=font)
+    plt.setp(axs2_title_text, size=9, weight='bold', color='red')
+    plt.setp(axs2_xlabel_text, size=7, weight='bold', color='black')
+    plt.setp(axs2_ylabel_text, size=7, weight='bold', color='black')
+    # 设置图例
+    didntLike = mlines.Line2D([], [], color='black', marker='.', markersize=6, label='didntLike')
+    smallDoses = mlines.Line2D([], [], color='orange', marker='.', markersize=6, label='smallDoses')
+    largeDoses = mlines.Line2D([], [], color='red', marker='.', markersize=6, label='largeDoses')
+    # 添加图例
+    axs[0][0].legend(handles=[didntLike, smallDoses, largeDoses])
+    axs[0][1].legend(handles=[didntLike, smallDoses, largeDoses])
+    axs[1][0].legend(handles=[didntLike, smallDoses, largeDoses])
+    # 显示图片
+    plt.show()
+
+def autoNorm(dataSet):
+    # 获取数据的最小值
+    minVals = dataSet.min(0)
+    # 获取数据的最大值
+    maxVals = dataSet.max(0)
+    # 最大值和最小值的范围
+    ranges = maxVals - minVals
+    # shape(dataSet)返回dataSet的矩阵行列数
+    normDataSet = np.zeros(np.shape(dataSet))
+    # numpy函数shape[0]返回dataSet的行数
+    m = dataSet.shape[0]
+    # 原始值减去最小值（x-xmin）
+    normDataSet = dataSet - np.tile(minVals, (m, 1))
+    # 差值处以最大值和最小值的差值（x-xmin）/（xmax-xmin）
+    normDataSet = normDataSet / np.tile(ranges, (m, 1))
+    # 归一化数据结果，数据范围，最小值
+    return normDataSet, ranges, minVals
+
+def datingClassTest():
+    # 打开文件名
+    filename = "datingTestSet.txt"
+    # 将返回的特征矩阵和分类向量分别存储到datingDataMat和datingLabels中
+    datingDataMat, datingLabels = file2matrix(filename)
+    # 取所有数据的10% hoRatio越小，错误率越低
+    hoRatio = 0.10
+    # 数据归一化，返回归一化数据结果，数据范围，最小值
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    # 获取normMat的行数
+    m = normMat.shape[0]
+    # 10%的测试数据的个数
+    numTestVecs = int(m * hoRatio)
+    # 分类错误计数
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        # 前numTestVecs个数据作为测试集，后m-numTestVecs个数据作为训练集
+        # k选择label数+1（结果比较好）
+        classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m,:],\
+                                     datingLabels[numTestVecs:m], 4)
+        print("分类结果:%d\t真实类别:%d" % (classifierResult, datingLabels[i]))
+        if classifierResult != datingLabels[i]:
+            errorCount += 1.0
+    print("错误率:%f%%" % (errorCount/float(numTestVecs)*100))
+
+def classifyPerson():
+    # 输出结果
+    resultList = ['讨厌', '有些喜欢', '非常喜欢']
+    # 三维特征用户输入
+    percentTats = float(input("玩视频游戏所消耗时间百分比："))
+    ffMiles = float(input("每年获得的飞行常客里程数："))
+    iceCream = float(input("每周消费的冰淇淋公升数："))
+    # 打开的文件名
+    filename = "datingTestSet.txt"
+    # 打开并处理数据
+    datingDataMat, datingLabels = file2matrix(filename)
+    # 训练集归一化
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    # 生成NumPy数组，测试集
+    inArr = np.array([percentTats, ffMiles, iceCream])
+    # 测试集归一化
+    norminArr = (inArr - minVals) / ranges
+    # 返回分类结果
+    classifierResult = classify0(norminArr, normMat, datingLabels, 4)
+    # 打印结果
+    print("你可能%s这个人" % (resultList[classifierResult - 1]))
+
--- KNN/TESTKNN.py
+++ KNN/TESTKNN.py
@@ -1,77 +1,34 @@
+import time
 import KNN
-import numpy as np
-import matplotlib.pyplot as plt
+def main():
+    # 获取程序运行时间
+    start = time.clock()
+    # 打开文件的名称
+    filename = "./datingTestSet.txt"
+    # 打开并处理数据
+    datingDataMat, datingLabels = KNN.file2matrix(filename)
+    # 训练集归一化
+    normDataset, ranges, minVals = KNN.autoNorm(datingDataMat)
+    KNN.datingClassTest()
+    # print(normDataset)
+    # print(ranges)
+    # print(minVals)
+    # showdatas(datingDataMat, datingLabels)
+    KNN.classifyPerson()
+    # print(datingDataMat)
+    # print(datingLabels)
+    # 创建数据集
+    # group, labels = createDataSet()
+    # 测试集
+    # test = [100,100]
+    # kNN分类
+    # test_class = classify0(test, group, labels, 3)
+    # 打印分类结果
+    # print(test_class)
+    end = time.clock()
+    # 打印程序运行时间
+    print('Running time: %f Seconds' % (end - start))
 
-if __name__ == "__main__":
-    data,labels = KNN.creatDataSet()
-    print(KNN.classify0([1,2],data,labels,3))
-    # print(data)
-    # x = data[:,0]
-    # y = data[:,1]
-    # print(x,y)
-    # plt.scatter(x,y,c = 'r',alpha=0.5,marker="x")
-    # plt.scatter(1,2,c = 'b',alpha=0.5,marker="o")
-    # plt.xlabel("X")
-    # plt.ylabel("Y")
-    # plt.title("movies")
-    # for i in range(len(x)):
-    #     plt.annotate(i,(x[i],y[i]))
-    # plt.show()
 
-    datingDataMat, datingLabels = KNN.file2matrix("./datingTestSet")
-    #设置汉字格式
-    font = FontProperties(fname=r"C:\Windows\Fonts\simsun.ttc", size=14)
-    #将figure画布分隔成1行1列，不共享x轴和y轴，fig画布的大小为（13，8）
-    # 当nrows=2，ncols=2时，代表fig画布被分为4个区域，axs[0][0]代表第一行第一个区域
-    fig,axs = plt.subplots(nrows = 2,ncols = 2,sharex = False,sharey = False,figsize = (13,8))
-    #获取datingLabels 的行数作为label的个数
-    #numberOfLabels = len(datingLabels)
-    #label 的颜色配置矩阵
-    LabelsColors = []
-    for i in datingLabels:
-        #didntLike
-        if i == 1:
-            LabelsColors.append('black')
-        #smallDoses
-        if i == 2:
-            LabelsColors.append('orange')
-        # largeDoses
-        if i == 3:
-            LabelsColors.append('orange')
-        # 画出散点图，以datingDataMat矩阵第一列为x，第二列为y，散点大小为15, 透明度为0.5
-        axs[0][0].scatter(x=datingDataMat[:, 0], y=datingDataMat[:, 1], color=LabelsColors, s=15, alpha=.5)
-        # 设置标题，x轴label， y轴label
-        axs0_title_text = axs[0][0].set_title(u'每年获得的飞行常客里程数与玩视频游戏所消耗时间占比', FontProperties=font)
-        axs0_xlabel_text = axs[0][0].set_xlabel(u'每年获得的飞行常客里程数', FontProperties=font)
-        axs0_ylabel_text = axs[0][0].set_ylabel(u'玩视频游戏所消耗时间占比', FontProperties=font)
-        plt.setp(axs0_title_text, size=9, weight='bold', color='red')
-        plt.setp(axs0_xlabel_text, size=7, weight='bold', color='black')
-        plt.setp(axs0_ylabel_text, size=7, weight='bold', color='black')
-        # 画出散点图，以datingDataMat矩阵第一列为x，第三列为y，散点大小为15, 透明度为0.5
-        axs[0][1].scatter(x=datingDataMat[:, 0], y=datingDataMat[:, 2], color=LabelsColors, s=15, alpha=.5)
-        # 设置标题，x轴label， y轴label
-        axs1_title_text = axs[0][1].set_title(u'每年获得的飞行常客里程数与每周消费的冰淇淋公升数', FontProperties=font)
-        axs1_xlabel_text = axs[0][1].set_xlabel(u'每年获得的飞行常客里程数', FontProperties=font)
-        axs1_ylabel_text = axs[0][1].set_ylabel(u'每周消费的冰淇淋公升数', FontProperties=font)
-        plt.setp(axs1_title_text, size=9, weight='bold', color='red')
-        plt.setp(axs1_xlabel_text, size=7, weight='bold', color='black')
-        plt.setp(axs1_ylabel_text, size=7, weight='bold', color='black')
-        # 画出散点图，以datingDataMat矩阵第二列为x，第三列为y，散点大小为15, 透明度为0.5
-        axs[1][0].scatter(x=datingDataMat[:, 1], y=datingDataMat[:, 2], color=LabelsColors, s=15, alpha=.5)
-        # 设置标题，x轴label， y轴label
-        axs2_title_text = axs[1][0].set_title(u'玩视频游戏所消耗时间占比与每周消费的冰淇淋公升数', FontProperties=font)
-        axs2_xlabel_text = axs[1][0].set_xlabel(u'玩视频游戏所消耗时间占比', FontProperties=font)
-        axs2_ylabel_text = axs[1][0].set_ylabel(u'每周消费的冰淇淋公升数', FontProperties=font)
-        plt.setp(axs2_title_text, size=9, weight='bold', color='red')
-        plt.setp(axs2_xlabel_text, size=7, weight='bold', color='black')
-        plt.setp(axs2_ylabel_text, size=7, weight='bold', color='black')
-        # 设置图例
-        didntLike = mlines.Line2D([], [], color='black', marker='.', markersize=6, label='didntLike')
-        smallDoses = mlines.Line2D([], [], color='orange', marker='.', markersize=6, label='smallDoses')
-        largeDoses = mlines.Line2D([], [], color='red', marker='.', markersize=6, label='largeDoses')
-        # 添加图例
-        axs[0][0].legend(handles=[didntLike, smallDoses, largeDoses])
-        axs[0][1].legend(handles=[didntLike, smallDoses, largeDoses])
-        axs[1][0].legend(handles=[didntLike, smallDoses, largeDoses])
-        # 显示图片
-        plt.show()
+if __name__ == '__main__':
+    main()
